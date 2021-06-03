156237506af6dff344a8a8a7981307d456e31438
rilith <bludoftenchiefs@protonmail.com>
2021-06-03 07:52:29 -0500

    commit tree c1032c718c5e1739f5fcf5d5b9d38068e72b401f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let personalData = {
+    'name' : 'Steve',
+    'email' : 'steve@steve.com'
+}
+
+const fetchData = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json'},
+    body: JSON.stringify(personalData)
+    }
+
+function submitData() {
+return fetch('http://localhost:3000/users', fetchData)
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object.id;
+  })
+  .catch(function(error) {
+    document.body.innerHTML = 'Unauthorized Access';
+  }); 
+}
